3d16fee0a88740f8c46c1eb31600916f2d99ad7b
zeff96 <zeffo1975@gmail.com>
2023-02-27 19:26:04 +0300

    Add all pr files for code review

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.css';
+import './components/add-delete.js';
+import './components/clear.js';
+import '@fortawesome/fontawesome-free/js/fontawesome.js';
+import '@fortawesome/fontawesome-free/js/solid.js';
+import '@fortawesome/fontawesome-free/js/regular.js';
+import '@fortawesome/fontawesome-free/js/brands.js';
