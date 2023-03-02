6bd6d8feb5fd7ca12822fd46f87a7d34c2dd7fe3
zeff96 <zeffo1975@gmail.com>
2023-03-02 19:35:17 +0300

    Add babel to support commonjs Add jest to allow application unit testing, Add code refactor to use class

--- src/index.js
+++ src/index.js
@@ -1,7 +1,51 @@
 import './index.css';
-import './components/add-delete.js';
-import './components/clear.js';
+import Node from './components/app';
 import '@fortawesome/fontawesome-free/js/fontawesome.js';
 import '@fortawesome/fontawesome-free/js/solid.js';
 import '@fortawesome/fontawesome-free/js/regular.js';
 import '@fortawesome/fontawesome-free/js/brands.js';
+
+const clearButton = document.querySelector('.btn');
+const list = document.querySelector('.list-items');
+const btn = document.querySelector('.add-todo');
+const addElem = document.querySelector('.add-list');
+
+const todoList = new Node();
+
+btn.addEventListener('click', () => {
+  if (addElem.value.trim()) {
+    todoList.createItems(addElem.value);
+    todoList.saveItems();
+    todoList.resetIndex();
+    todoList.render(list);
+    addElem.value = '';
+  }
+});
+
+addElem.addEventListener('keydown', (e) => {
+  if (e.key === "Enter") {
+    if (addElem.value.trim()) {
+      todoList.createItems(addElem.value);
+      todoList.saveItems();
+      todoList.resetIndex();
+      todoList.render(list);
+      addElem.value = '';
+    }
+  }
+});
+
+list.addEventListener('click', (e) => {
+  const target = e.target;
+  if (target.className === 'deletebtn') {
+    const id = Number(target.parentNode.id);
+    todoList.deleteItems(id);
+    todoList.resetIndex();
+    todoList.saveItems();
+    todoList.render(list);
+  };
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  todoList.savedItems();
+  todoList.render(list);
+});
